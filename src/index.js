3bcfbbfee60d68e1b2a0d478efca6b777db8be3f
Yuliana Velez <velezrodriguez2001@gmail.com>
2023-07-12 21:44:15 -0500

    Fixed linters

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import './style.css';
+
 const container = document.querySelector('#container');
 const toDo = [
   {
-    description : "task number 1",
-    completed : false,
-    index : 1, 
+    description: 'task number 1',
+    completed: false,
+    index: 1,
   },
   {
-    description : "task number 2",
-    completed : true,
-    index : 2, 
+    description: 'task number 2',
+    completed: true,
+    index: 2,
   },
   {
-    description : "task number 3",
-    completed : false,
-    index : 3, 
-  }
-]
+    description: 'task number 3',
+    completed: false,
+    index: 3,
+  },
+];
 
 const createToDo = (toDo) => {
   const toDoItem = document.createElement('article');
@@ -27,11 +28,11 @@ const createToDo = (toDo) => {
   <input class="description" value="${toDo.description}">
   <div class="dragger">&#8942;</div>
   <div class="deleter hidden">&#128465;</div>
-  `
+  `;
   container.appendChild(toDoItem);
-}
+};
 
-toDo.sort((a,b) => a.index-b.index);
+toDo.sort((a, b) => a.index - b.index);
 for (let i = 0; i < toDo.length; i += 1) {
-  createToDo(toDo[i])
+  createToDo(toDo[i]);
 }
