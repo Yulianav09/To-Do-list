010e3c60d5088da0ae95e8c2f92d1d78a9b64fb9
Yuliana Velez <velezrodriguez2001@gmail.com>
2023-07-13 20:19:30 -0500

    Added event listener to the checkbox

--- src/toDoList.js
+++ src/toDoList.js
@@ -1,3 +1,4 @@
+import toggleCheck from './checkBox.js';
 import { saveLocalStorage } from './localStorage.js';
 
 export default class ToDoList {
@@ -6,54 +7,62 @@ export default class ToDoList {
     this.list = [];
   }
 
-createToDo = (toDo) => {
-  const toDoItem = document.createElement('form');
-  toDoItem.classList.add('toDoItem');
+  createToDo = (toDo) => {
+    const toDoItem = document.createElement('form');
+    toDoItem.classList.add('toDoItem');
+    toDo.markUp = toDoItem;
 
-  toDoItem.innerHTML = `
-  <input type="checkbox" ${toDo.completed ? 'checked' : ''}>
-  <input class="description" value="${toDo.description}">
-  <div class="dragger">&#8942;</div>
-  <div class="deleter hidden">&#128465;</div>
-  `;
-  const description = toDoItem.querySelector('.description');
-  const dragger = toDoItem.querySelector('.dragger');
-  const deleter = toDoItem.querySelector('.deleter');
+    toDoItem.innerHTML = `
+    <input class="checkBox" type="checkbox" ${toDo.completed ? 'checked' : ''}>
+    <input class="description ${toDo.completed ? 'checkedTask' : ''}" value="${toDo.description}">
+    <div class="dragger">&#8942;</div>
+    <div class="deleter hidden">&#128465;</div>
+    `;
+    const description = toDoItem.querySelector('.description');
+    const dragger = toDoItem.querySelector('.dragger');
+    const deleter = toDoItem.querySelector('.deleter');
+    const checkBox = toDoItem.querySelector('.checkBox');
 
-  deleter.addEventListener('click', (e) => {
-    this.removeToDo(e, toDo);
-    this.reorderIndex();
-    saveLocalStorage(this);
-  });
+    deleter.addEventListener('click', (e) => {
+      this.removeToDo(e, toDo);
+      this.reorderIndex();
+      saveLocalStorage(this);
+    });
 
-  description.addEventListener('change', (e) => {
-    toDo.description = e.target.value;
-    saveLocalStorage(this);
-  });
+    description.addEventListener('change', (e) => {
+      toDo.description = e.target.value;
+      saveLocalStorage(this);
+    });
 
-  description.addEventListener('focus', () => {
-    dragger.classList.add('hidden');
-    deleter.classList.remove('hidden');
-  });
+    checkBox.addEventListener('change', (e) => {
+      toDo.completed = e.target.checked;
+      toggleCheck(description, toDo.completed);
+      saveLocalStorage(this);
+    });
 
-  description.addEventListener('blur', () => {
-    setTimeout(() => {
-      dragger.classList.remove('hidden');
-      deleter.classList.add('hidden');
-    }, 150);
-  });
-  this.container.appendChild(toDoItem);
-}
+    description.addEventListener('focus', () => {
+      dragger.classList.add('hidden');
+      deleter.classList.remove('hidden');
+    });
 
-removeToDo = (e, toDoData) => {
-  e.target.parentElement.remove();
-  this.list = this.list.filter((toDoFilter) => toDoFilter !== toDoData);
-}
+    description.addEventListener('blur', () => {
+      setTimeout(() => {
+        dragger.classList.remove('hidden');
+        deleter.classList.add('hidden');
+      }, 150);
+    });
+    this.container.appendChild(toDoItem);
+  }
 
-reorderIndex = () => {
-  this.list = this.list.map((element, index) => {
-    element.index = index + 1;
-    return element;
-  });
-}
+  removeToDo = (e, toDoData) => {
+    e.target.parentElement.remove();
+    this.list = this.list.filter((toDoFilter) => toDoFilter !== toDoData);
+  }
+
+  reorderIndex = () => {
+    this.list = this.list.map((element, index) => {
+      element.index = index + 1;
+      return element;
+    });
+  }
 }
