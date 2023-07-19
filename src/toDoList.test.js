1d1258f90df4e58a53f2b2b03b448e4c3f3128c6
Yuliana Velez <velezrodriguez2001@gmail.com>
2023-07-19 18:03:09 -0500

    Added test cases

---
+++ src/toDoList.test.js
@@ -0,0 +1,36 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import ToDoList from './toDoList.js';
+  document.body.innerHTML =`
+      <div id="container"></div>`;
+  const container = document.querySelector('#container');
+  const myToDo = new ToDoList(container);
+
+  describe('Adding', () => {
+
+  console.log (localStorage)
+  const myToDo = new ToDoList(container);
+
+  const item = {
+    description : "text",
+    completed : false,
+    index : 0,
+    markUp : null,
+  }
+
+    test('Test if instance of', () => {
+      // expect(myToDo).toBeInstanceOf(ToDoList)
+      expect(container.innerHTML).toHaveLength(0);
+    })
+
+    test('if HTML is added to the container', () => {
+      myToDo.createToDo(item);
+      expect(container.innerHTML).not.toHaveLength(0);
+    });
+
+    test('test removing elements', () =>{
+      myToDo.removeToDo
+    })
+  });
