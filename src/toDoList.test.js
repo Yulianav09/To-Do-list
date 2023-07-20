53b9b4d9a60d4a6f1b38c9d56db9596f8e523567
Yuliana Velez <velezrodriguez2001@gmail.com>
2023-07-20 10:41:21 -0500

    Added test to remove element

--- src/toDoList.test.js
+++ src/toDoList.test.js
@@ -2,35 +2,36 @@
  * @jest-environment jsdom
  */
 
+import { describe } from 'yargs';
 import ToDoList from './toDoList.js';
   document.body.innerHTML =`
       <div id="container"></div>`;
   const container = document.querySelector('#container');
   const myToDo = new ToDoList(container);
-
-  describe('Adding', () => {
-
-  console.log(localStorage);
-  const myToDo = new ToDoList(container);
-
   const item = {
     description : "text",
     completed : false,
     index : 0,
     markUp : null,
   }
+  myToDo.list.push(item);
+
+  describe('Adding', () => {
 
     test('Test if container is empty', () => {
       // expect(myToDo).toBeInstanceOf(ToDoList)
       expect(container.innerHTML).toHaveLength(0);
     })
 
-    test('if HTML is added to the container', () => {
+    test('Test if HTML is added to the container', () => {
       myToDo.createToDo(item);
       expect(container.innerHTML).not.toHaveLength(0);
     });
 
-    // test('test removing elements', () =>{
-    //   myToDo.removeToDo
-    // })
   });
+
+  describe('Remove elements', () => {
+    test('test removing elements', () =>{
+      myToDo.removeToDo
+    })
+  })
