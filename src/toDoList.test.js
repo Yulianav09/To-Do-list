33f631b5ba892e0eecc000060a923cb4f706b683
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-07-20 12:08:18 -0500

    Fix linters

--- src/toDoList.test.js
+++ src/toDoList.test.js
@@ -3,40 +3,39 @@
  */
 
 import ToDoList from './toDoList.js';
-  document.body.innerHTML =`
+
+document.body.innerHTML = `
       <div id="container"></div>`;
-  const container = document.querySelector('#container');
-  const myToDo = new ToDoList(container);
-  const item = {
-    description : "text",
-    completed : false,
-    index : 0,
-    markUp : null,
-  }
-  myToDo.list.push(item);
+const container = document.querySelector('#container');
+const myToDo = new ToDoList(container);
+const item = {
+  description: 'text',
+  completed: false,
+  index: 0,
+  markUp: null,
+};
+myToDo.list.push(item);
 
-  describe('Adding', () => {
-
-    test('Test if container is empty', () => {
-      // expect(myToDo).toBeInstanceOf(ToDoList)
-      expect(container.innerHTML).toHaveLength(0);
-    })
-
-    test('Test if HTML is added to the container', () => {
-      myToDo.createToDo(item);
-      expect(container.innerHTML).not.toHaveLength(0);
-    });
-
+describe('Adding', () => {
+  test('Test if container is empty', () => {
+    // expect(myToDo).toBeInstanceOf(ToDoList)
+    expect(container.innerHTML).toHaveLength(0);
   });
 
-  describe('Remove elements', () => {
-    test('test removing elements', () =>{
-      const button = document.querySelector('.deleter')
-      const event = {target : button}
-      myToDo.removeToDo(event, item)
-      expect(container.innerHTML).toHaveLength(0);
-    })
-    test('Test container', () => {
-      expect(container.innerHTML.length).not.toBeGreaterThan(0)
-    })
-  })
+  test('Test if HTML is added to the container', () => {
+    myToDo.createToDo(item);
+    expect(container.innerHTML).not.toHaveLength(0);
+  });
+});
+
+describe('Remove elements', () => {
+  test('test removing elements', () => {
+    const button = document.querySelector('.deleter');
+    const event = { target: button };
+    myToDo.removeToDo(event, item);
+    expect(container.innerHTML).toHaveLength(0);
+  });
+  test('Test container', () => {
+    expect(container.innerHTML.length).not.toBeGreaterThan(0);
+  });
+});
