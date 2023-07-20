2c0872a4b30f3e98c5c9093b37ea10483895edae
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-07-20 14:51:50 -0500

    Add update state test in check box

--- src/update.test.js
+++ src/update.test.js
@@ -18,9 +18,9 @@ const item = {
 };
 myToDo.list.push(item);
 myToDo.createToDo(item);
+const changeEvent = new Event('change') // Created and event to trigger the Event listener of the description input
 
-describe('Update description', () => {
-  const changeEvent = new Event('change') // Created and event to trigger the Event listener of the description input
+describe('edit description', () => {
   const description = document.querySelector('.description')
   description.value = 'new task' // changed the value of the description input in the DOM
   description.dispatchEvent(changeEvent) // Executed the event to trigger the event listener inside the myToDO object
@@ -28,5 +28,15 @@ describe('Update description', () => {
   test('Test if description changed',() => {
     expect(myToDo.list[0].description).toBe('new task')
   })
+  
+})
 
+describe('update state', () => {
+  const checkBox = document.querySelector('.checkBox')
+  checkBox.checked = true // changed the state of the checkBox input in the DOM
+  checkBox.dispatchEvent(changeEvent) // Executed the event to trigger the event listener inside the myToDO object
+  
+  test('Test if state changed',() => {
+    expect(myToDo.list[0].completed).toBe(true)
+  })
 })
